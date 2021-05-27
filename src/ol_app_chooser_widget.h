2d0ee22591a8c8535c178e0d81a48d43e2d09cec
Daniel Șerbănescu <daniel@serbanescu.dk>
2021-05-27 20:56:06 +0200

    Removed deprecated G_TYPE_INSTANCE_GET_PRIVATE and its code connections.

--- src/ol_app_chooser_widget.h
+++ src/ol_app_chooser_widget.h
@@ -3,7 +3,7 @@
  * Copyright (C) 2011  Tiger Soldier
  *
  * This file is part of OSD Lyrics.
- * 
+ *
  * OSD Lyrics is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -15,13 +15,23 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OSD Lyrics.  If not, see <https://www.gnu.org/licenses/>. 
+ * along with OSD Lyrics.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef _OL_APP_CHOOSER_WIDGET_H_
 #define _OL_APP_CHOOSER_WIDGET_H_
 
 #include <gtk/gtk.h>
+
+typedef struct {
+  GtkWidget widget;
+  GtkTable parent;
+} OlAppChooserWidget;
+
+typedef struct {
+  GtkWidgetClass widget_class;
+  GtkTableClass parent_class;
+} OlAppChooserWidgetClass;
 
 #define OL_TYPE_APP_CHOOSER_WIDGET                                      \
   ol_app_chooser_widget_get_type ()
@@ -36,27 +46,13 @@
                               OL_TYPE_APP_CHOOSER_WIDGET,               \
                               OlAppChooserWidgetClass))
 
-typedef struct _OlAppChooserWidget OlAppChooserWidget;
-typedef struct _OlAppChooserWidgetClass OlAppChooserWidgetClass;
-
-struct _OlAppChooserWidget
-{
-  GtkTable parent;
-};
-
-struct _OlAppChooserWidgetClass
-{
-  GtkTableClass parent_class;
-};
-
 GType ol_app_chooser_widget_get_type (void);
-
 GtkWidget *ol_app_chooser_widget_new (void);
 
-/** 
+/**
  * Sets the app infos to the app chooser.
- * 
- * @param chooser 
+ *
+ * @param chooser
  * @param app_list A GList of GAppInfo*.
  * @param n_columns The number of columns to show the apps. If it is set to 0, the
  *                  number of columns will be calculated according to the number
@@ -66,13 +62,13 @@ void ol_app_chooser_widget_set_app_list (OlAppChooserWidget *chooser,
                                          GList *app_list,
                                          guint n_columns);
 
-/** 
+/**
  * Gets the number of columns of displayed apps.
  *
  * The number is guaranteed to be non-zero after setting the app_list.
- * @param chooser 
- * 
- * @return 
+ * @param chooser
+ *
+ * @return
  */
 guint ol_app_chooser_widget_get_columns (OlAppChooserWidget *chooser);
 #endif /* _OL_APP_CHOOSER_WIDGET_H_ */
