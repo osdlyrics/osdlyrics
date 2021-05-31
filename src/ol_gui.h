a73fa805ae4d2f6b7fac4e46deea86eada2f110d
Daniel Șerbănescu <daniel@serbanescu.dk>
2021-05-31 15:05:46 +0200

    Fixed gtk deprecation warnings for ol_gui

--- src/ol_gui.h
+++ src/ol_gui.h
@@ -3,7 +3,7 @@
  * Copyright (C) 2009-2011  Tiger Soldier <tigersoldier@gmail.com>
  *
  * This file is part of OSD Lyrics.
- * 
+ *
  * OSD Lyrics is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -15,19 +15,20 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OSD Lyrics.  If not, see <https://www.gnu.org/licenses/>. 
+ * along with OSD Lyrics.  If not, see <https://www.gnu.org/licenses/>.
  */
 #ifndef _OL_GUI_H_
 #define _OL_GUI_H_
 
-#include <gtk/gtk.h>
+#include <gtk/gtkwidget.h>
+#include <gtk/gtkbuilder.h>
 
-/** 
+/**
  * @brief Gets a widget in Glade file by name
- * 
+ *
  * @param name name of the widget
- * 
- * @return 
+ *
+ * @return
  */
 GtkWidget *ol_gui_get_widget (const char *name);
 
