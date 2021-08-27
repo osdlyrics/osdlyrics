3648cd006a783d4d64405843cd3e9dd65d6c2c30
poly000 <34085039+poly000@users.noreply.github.com>
2021-08-27 20:26:59 +0800

    Update netease.py

--- lyricsources/netease/translation_attempt/netease.py
+++ lyricsources/netease/translation_attempt/netease.py
@@ -25,7 +25,7 @@ class NeteaseSource(BaseLyricSourcePlugin):
     """
 
     def __init__(self):
-        super().__init__(id='netease', name=_('Netease (tr)'))
+        super().__init__(id='netease-tr', name=_('Netease (tr)'))
 
     def do_search(self, metadata):
         # type: (osdlyrics.metadata.Metadata) -> List[SearchResult]
