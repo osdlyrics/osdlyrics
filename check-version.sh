46b05010d3e3031ea057d94db25fa59be3db7303
PedroHLC ☭ <root@pedrohlc.com>
2022-07-07 17:53:53 -0300

    configure: could this be the problem (2)

--- check-version.sh
+++ check-version.sh
@@ -7,8 +7,8 @@ GIT="${GIT:-git}"
 OL_VERSION="${OL_VERSION:-0.5.11}"
 
 if test -d '.git' && command -v "$GIT" &> /dev/null; then
-	"$GIT" describe --always --tags
+	exec "$GIT" describe --always --tags
 else
-	echo "$OL_VERSION"
+	exec echo "$OL_VERSION"
 fi
 
