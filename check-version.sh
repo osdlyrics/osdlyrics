2c955a9bf2ae2352e5429cf3934c3b9d4215a3b2
PedroHLC ☭ <root@pedrohlc.com>
2022-07-07 16:34:49 -0300

    ci: Fuki the CI

--- check-version.sh
+++ check-version.sh
@@ -4,10 +4,11 @@ set -e
 
 DIR=`dirname $0`
 GIT="${GIT:-git}"
-OL_VERSION="${GIT:-0.5.11}"
+OL_VERSION="${OL_VERSION:-0.5.11}"
 
 if test -d '.git' && command -v "$GIT" &> /dev/null; then
-	"$GIT" describe --always --tags
+	#"$GIT" describe --always --tags
+	echo "$OL_VERSION"
 else
 	echo "$OL_VERSION"
 fi
