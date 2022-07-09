70165c56ef426f51f212a31c1e3d34d6a2d3572e
Pedro Lara Campos <pedro.laracampos@gmail.com>
2022-07-09 10:26:46 -0300

    configure: use git describe for version (#117)

---
+++ check-version.sh
@@ -0,0 +1,15 @@
+#h!/usr/bin/env sh
+
+GIT="${GIT:-git}"
+OL_VERSION="${OL_VERSION:-0.5.11}"
+
+if [ ! "$("$GIT" rev-parse --show-prefix 2>/dev/null)" ]; then
+  _GIT_VERSION="$("$GIT" describe --always --tags 2>/dev/null)"
+  if [ "$_GIT_VERSION" ]; then
+    echo "$_GIT_VERSION"
+  else
+    echo "$OL_VERSION"
+  fi
+else
+  echo "$OL_VERSION"
+fi
