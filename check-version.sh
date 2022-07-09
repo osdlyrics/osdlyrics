a4af48807e3fee01671932387cb9a9debfbd8b17
PedroHLC ☭ <root@pedrohlc.com>
2022-07-09 10:21:10 -0300

    check-version: check for empty git-describe (mocked)

--- check-version.sh
+++ check-version.sh
@@ -1,9 +1,15 @@
-#!/usr/bin/env sh
+#h!/usr/bin/env sh
 
 GIT="${GIT:-git}"
 OL_VERSION="${OL_VERSION:-0.5.11}"
 
-if [ "$("$GIT" rev-parse --show-prefix 2>/dev/null)" ] ||
-   ! "$GIT" describe --always --tags 2>/dev/null; then
-	echo "$OL_VERSION"
+if [ ! "$("$GIT" rev-parse --show-prefix 2>/dev/null)" ]; then
+  _GIT_VERSION="$("$GIT" describe --always --tags 2>/dev/null)"
+  if [ "$_GIT_VERSION" ]; then
+    echo "$_GIT_VERSION"
+  else
+    echo "$OL_VERSION"
+  fi
+else
+  echo "$OL_VERSION"
 fi
